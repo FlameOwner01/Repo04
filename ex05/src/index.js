73a138deb067488bad7924d203d69a539f7cefdb
FlameOwner <73409497+FlameOwner01@users.noreply.github.com>
2021-09-17 02:34:01 +0200

    second commit

--- ex05/src/index.js
+++ ex05/src/index.js
@@ -1,7 +1,5 @@
 function main(numOne,numTwo){
     // Only change code below this line
-    var numOne;
-    var numTwo;
     var numOne1 = parseFloat(numOne);
     var numTwo1 = parseFloat(numTwo);
     var product = numOne1*numTwo1;
