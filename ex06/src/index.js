8c2b151cebc9aa8d80ee8cfa8d8b901558ee2ceb
FlameOwner <73409497+FlameOwner01@users.noreply.github.com>
2021-09-17 02:34:25 +0200

    Update index.js

--- ex06/src/index.js
+++ ex06/src/index.js
@@ -1,7 +1,5 @@
 function main(numOne,numTwo){
     // Only change code below this line
-    var numOne;
-    var numTwo;
     var numOne1 = parseFloat(numOne);
     var numTwo1 = parseFloat(numTwo);
     var myQuotient = parseFloat(numOne1/numTwo1);
